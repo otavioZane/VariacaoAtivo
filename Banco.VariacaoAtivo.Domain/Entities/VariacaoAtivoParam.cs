c6111f0f6955d990726b5c1ada8cb6e743bbaa09
otavioZane <otavio.rzane@gmail.com>
2023-03-30 11:21:36 -0300

    first commit

---
+++ Banco.VariacaoAtivo.Domain/Entities/VariacaoAtivoParam.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco.VariacaoAtivo.Domain.Entities {
+    public class VariacaoAtivoParam {
+
+        public VariacaoAtivoParam()
+        {
+            
+        }
+
+        public int Day { get; set; }
+        public DateOnly Date { get; set; }
+        public decimal Value { get; set; }
+        public decimal VariationFirstDate { get; set; }
+        public decimal VariationRelationD1 { get; set; }
+    }
+}
